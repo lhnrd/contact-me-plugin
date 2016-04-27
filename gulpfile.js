2e57e52a85a50f9fe73c40c4d2c9ee8a69afd1c2
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2016-04-27 13:10:35 -0300

    Divide gulpfile into two servers
    One test server and one server for the index.html preview

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,50 @@ var p = require('gulp-load-plugins')();
 var wiredep = require('wiredep');
 var autoprefixer = require('autoprefixer');
 var csso = require('postcss-csso');
-var bs = require('browser-sync').create();
+var bs = require('browser-sync');
+var mock = require('json-mock');
 
 var path = {
   html: ['src/**/*.html'],
   script: ['src/**/*.js'],
   style: ['src/**/*.css'],
-  test: ['test/**/*.js'],
+  test: ['test/**/*.js', '!test/jasmine/**/*.js'],
   output: 'dist'
 };
+
+var bsTest;
+var bsServer;
 
 gulp.task('clean', function() {
   gulp.src(path.output).pipe(p.clean());
 });
 
 gulp.task('lint', function() {
-  return gulp.src(path.script)
+  return gulp.src(path.script.concat(path.test))
     .pipe(p.eslint())
-    .pipe(p.eslint.format())
-    .pipe(p.eslint.failAfterError());
+    .pipe(p.eslint.format());
 });
 
 gulp.task('test', function() {
-  return gulp.src(path.test)
-    .pipe(p.jasmine())
-    .on('error', p.notify.onError({
-      title: 'Jasmine Test Failed',
-      message: 'One or more tests failed, see the cli for details.'
-    }));
+  var api = mock.create();
+  api.use(mock.router('test/db.json'));
+  api.listen(3005);
+
+  bsTest = bs.create('Server test');
+  bsTest.init({
+    ui: {port: 3003},
+    server: {
+      baseDir: ['test'],
+      index: 'SpecRunner.html',
+      routes: {
+        '/lib': 'lib',
+        '/src': 'src/js'
+      }
+    },
+    port: 3002
+  });
+
+  gulp.watch(path.test.concat(path.script)).on('change', bsTest.reload);
 });
 
 gulp.task('inject-bower', function() {
@@ -71,18 +87,29 @@ gulp.task('build', function() {
     .pipe(gulp.dest(path.output));
 });
 
+gulp.task('watch', function() {
+  gulp.watch(path.style).on('change', bsServer.reload);
+  gulp.watch(path.script, ['lint', 'test']).on('change', bsServer.reload);
+  gulp.watch(path.html).on('change', bsServer.reload);
+});
+
 gulp.task('serve', ['inject-bower'], function() {
-  bs.init({
+  bsServer = bs.create('Server');
+  bsServer.init({
+    ui: {port: 3001},
     server: {
       baseDir: ['src'],
       index: 'index.html',
       routes: {
         '/lib': 'lib'
       }
-    }
+    },
+    port: 3000
   });
 
-  gulp.watch(path.style).on('change', bs.reload);
-  gulp.watch(path.script, ['lint', 'test']).on('change', bs.reload);
-  gulp.watch(path.html).on('change', bs.reload);
+  gulp.watch(path.style).on('change', bsServer.reload);
+  gulp.watch(path.script, ['lint']).on('change', bsServer.reload);
+  gulp.watch(path.html).on('change', bsServer.reload);
 });
+
+gulp.task('default', ['serve', 'test']);
